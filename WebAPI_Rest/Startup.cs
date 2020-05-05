fe558ffa482c09bec61c0dda9c75b0368126bb6e
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-05-05 18:16:39 -0300

    Instalando Entity Framework Core e Configuração Inicial

--- WebAPI_Rest/Startup.cs
+++ WebAPI_Rest/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebAPI_Rest.Models;
 
 namespace WebAPI_Rest
 {
@@ -24,6 +26,10 @@ namespace WebAPI_Rest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<Context>(options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection"), builder =>
+ builder.MigrationsAssembly("WebAPI_Rest")));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
