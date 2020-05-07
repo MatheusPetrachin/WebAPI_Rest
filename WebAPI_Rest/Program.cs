01b5750870aae870253f43b35eadfae87df3ad5d
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-05-07 09:55:24 -0300

    web.config

--- WebAPI_Rest/Program.cs
+++ WebAPI_Rest/Program.cs
@@ -19,6 +19,7 @@ namespace WebAPI_Rest
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseIISIntegration();
     }
 }
